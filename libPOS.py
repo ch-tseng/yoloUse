9f677235fd9ff39632c0bd8c60b095a4ea136ca3
ch-tseng <ch.tseng@sunplusit.com>
2018-12-31 09:52:41 +0800

    update

--- libPOS.py
+++ libPOS.py
@@ -116,3 +116,21 @@ class desktop:
         #cv2.imshow("Thresh", thresh)
 
         return counts
+
+def printText(self, txt, bg, color=(0,255,0,0), size=0.7, pos=(0,0), type="Chinese"):
+    (b,g,r,a) = color
+
+    if(type=="English"):
+        ## Use cv2.FONT_HERSHEY_XXX to write English.
+        cv2.putText(bg,  txt, pos, cv2.FONT_HERSHEY_SIMPLEX, size,  (b,g,r), 2, cv2.LINE_AA)
+
+    else:
+        ## Use simsum.ttc to write Chinese.
+        fontpath = "./wt009.ttf"
+        font = ImageFont.truetype(fontpath, size*50)
+        img_pil = Image.fromarray(bg)
+        draw = ImageDraw.Draw(img_pil)
+        draw.text(pos,  txt, font = font, fill = (b, g, r, a))
+        bg = np.array(img_pil)
+
+    return bg
