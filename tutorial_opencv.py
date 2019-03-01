1fc2d2acfefcc29e0b63fc6efec2bad04af2b48f
mis <ch.tseng@sunplusit.com>
2019-03-01 22:21:50 +0800

    update

--- tutorial_opencv.py
+++ tutorial_opencv.py
@@ -4,13 +4,16 @@ import imutils
 import time
 
 yolo = opencvYOLO(modeltype="yolov3", \
-    objnames="/media/sf_ShareFolder/cfg.roadYolo/obj.names", \
-    weights="/media/sf_ShareFolder/cfg.roadYolo/yolov3_30000.weights",\
-    cfg="/media/sf_ShareFolder/cfg.roadYolo/yolov3.cfg")
+    objnames="/media/sf_VMshare/cfg.hand_gesture/obj.names", \
+    weights="/media/sf_VMshare/cfg.hand_gesture/weights/yolov3-tiny_60000.weights",\
+    cfg="/media/sf_VMshare/cfg.hand_gesture/yolov3-tiny.cfg")
 
 inputType = "video"  # webcam, image, video
-media = "/media/sf_ShareFolder/road.mp4"
-video_out = "/media/sf_ShareFolder/road.avi"
+media = "/media/sf_VMshare/hand1.MOV"
+write_video = False
+video_out = "/media/sf_VMshare/out_hand1_yolov3.avi"
+output_rotate = True
+rotate = 180
 
 start_time = time.time()
 
@@ -39,29 +42,44 @@ if __name__ == "__main__":
             width = int(INPUT.get(cv2.CAP_PROP_FRAME_WIDTH))   # float
             height = int(INPUT.get(cv2.CAP_PROP_FRAME_HEIGHT)) # float
 
-            fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-            out = cv2.VideoWriter(video_out,fourcc, 30.0, (int(width),int(height)))
+            if(write_video is True):
+                fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+                out = cv2.VideoWriter(video_out,fourcc, 30.0, (int(width),int(height)))
 
         frameID = 0
+
         while True:
             hasFrame, frame = INPUT.read()
             # Stop the program if reached end of video
             if not hasFrame:
                 print("Done processing !!!")
-                print("--- %s seconds ---" % (time.time() - start_time))
+                # End time
+                end = time.time()
+                # Time elapsed
+                seconds = end - start_time
+                print ("Time taken : {0} seconds".format(seconds))
+ 
+                # Calculate frames per second
+                fps  = frameID / seconds;
+                print ("Estimated frames per second : {0}".format(fps))
                 break
 
-            yolo.getObject(frame, labelWant="", drawBox=True, bold=1, textsize=0.6, bcolor=(0,0,255), tcolor=(255,255,255))
+            if(output_rotate is True):
+                frame = imutils.rotate(frame, rotate)
+
+            yolo.getObject(frame, labelWant="", drawBox=True, bold=2, textsize=1.2, bcolor=(0,255,0), tcolor=(0,0,255))
             print ("Object counts:", yolo.objCounts)
             #yolo.listLabels()
             print("classIds:{}, confidences:{}, labelName:{}, bbox:{}".\
                 format(len(yolo.classIds), len(yolo.scores), len(yolo.labelNames), len(yolo.bbox)) )
             cv2.imshow("Frame", imutils.resize(frame, width=850))
 
-            if(video_out!=""):
+            if(write_video is True):
                 out.write(frame)
 
             k = cv2.waitKey(1)
             if k == 0xFF & ord("q"):
-                out.release()
+                if(write_video is True):
+                    out.release()
+
                 break
